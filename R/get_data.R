dcb8762930871dc85faa9ce4846cb02855b26529
ember-fbijaksana <faza.bijaksana@emberadvisors.com.au>
2022-12-16 17:05:33 +1100

    Code for scraping childcare and rent data

--- R/get_data.R
+++ R/get_data.R
@@ -1,20 +1,82 @@
-# function to scrape relevant data
-get_vacancies <- function(url){
+
+## This function scrapes the childcare website to collect vacancies data, avg cost and avg quality
+get_childcare_data <- function(link, postcode){
   
-  rvest::read_html(url) %>%
+  session <- polite::bow(link)
+  
+  web <- session %>%
+    polite::scrape() %>%
     rvest::html_text() %>%
-    str_remove_all("\r|\t|\n") %>%
+    str_remove_all("\r|\t|\n")
+  
+  vacancies <- web %>%
     str_match_all("Number of Centres\\s*(.*?)\\s*View") %>%
+    purrr::pluck(1,2) %>%
+    str_replace("Centres with Vacancies", "_") %>%
+    str_split("_")
+  
+  vacancies <-tibble(total_centres = vacancies %>%
+                       pluck(1,1) %>%
+                       as.numeric(),
+                     vacancies = vacancies %>%
+                       pluck(1,2) %>%
+                       as.numeric()) %>%
+    mutate(vacancy_rate = vacancies/total_centres)
+  
+  avg_cost <- web %>%
+    str_match("\\$([0-9,.]+)") %>%
+    purrr::pluck(2) %>%
+    as.numeric()
+  
+  cost <- tibble(avg_cost = avg_cost)
+  
+  quality <- web %>%
+    str_match_all("NQS Rating is\\s*(.*?)\\s*National Quality Standard") %>%
     purrr::pluck(1,2)
+  
+  quality <- tibble(avg_quality = quality) %>%
+    mutate(avg_quality = if_else(str_detect(tolower(avg_quality), "provisional"), "Provisional - not yet assessed", avg_quality))
+  
+  tibble(postcode = postcode) %>%
+    bind_cols(vacancies, cost, quality)
   
 }
 
-get_avg_cost <- function(url){
+## This function scrapes the SQM website to collect median weekly rent
+get_rent <- function(link, postcode) {
   
-  rvest::read_html(url) %>%
-    rvest::html_text() %>%
-    str_remove_all("\r|\t|\n") %>%
-    str_match("\\$([0-9,.]+)") %>%
-    purrr::pluck(2)
+  session <- polite::bow(link, delay = 7)
+  
+  # scrape rent table using the combined median rent 
+  df <- session %>%
+    polite::scrape() %>%
+    html_table() %>%
+    pluck(1) %>%
+    slice_tail() %>%
+    select(3, 7)
+  
+  names(df) <- c("median_rent", "12_month_change")
+  
+  # get week ending date
+  date <- session %>%
+    polite::scrape() %>%
+    html_table() %>%
+    pluck(1) %>%
+    pluck(1,2) %>%
+    str_extract_all("\\d{2} [a-zA-Z]{3} \\d{4}") %>%
+    pluck(1) %>%
+    as.Date(format = "%d %b %Y")
+  
+  df %>%
+    mutate(week_ending = date,
+           postcode = postcode) %>%
+    select(postcode, week_ending, median_rent, `12_month_change`)
   
 }
+  
+
+
+
+
+
+
--- childcare_scrape.R
+++ childcare_scrape.R
@@ -1,28 +1,66 @@
 ##########
-## Script to scrape childcare data from https://www.careforkids.com.au/child-care
+## Script to scrape data for RNSW indicators
 ## Author: Faza Bijaksana
 ## Date: 20221209
 ##########
 
 
 ### Setup
-# Packages
+## Packages
 library(tidyverse)
 library(glue)
 library(rvest)
+library(polite)
 library(absmapsdata) #to install run remotes::install_github("wfmackey/absmapsdata")
 
-# get postcodes we are interested in
+## Read functions
+source("./R/get_data.R")
+
+## Get postcodes we are interested in
+# ABS correspondence for postcode to LGA so we can get postcodes for the relevant LGAs in Central West 
 corro <- absmapsdata::get_correspondence_absmaps("postcode", 2019, "lga", 2019)
+
+# Make list of relevant LGAs in Central West directorate
 central_west_lga <- c("bathurst|blayney|cabonne|cowra|forbes|lachlan|lithgow|oberon|orange|parkes|weddin")
+
+# Filter postcodes to only those in central_west_lga
 central_west_abs <- corro %>%
   filter(str_detect(tolower(LGA_NAME_2019), central_west_lga) & !str_detect(tolower(LGA_NAME_2019), "shire"))
-
-# url
-childcare_url <- "https://www.careforkids.com.au/child-care/"
-
-# make tibble with url to scrape
-urls <- tibble(postcode = corro$POSTCODE_2019) %>%
+postcodes <- corro$POSTCODE_2019 %>%
   unique()
-urls <- urls %>%
-  mutate(url = glue::glue("{childcare_url}{postcode}"))
+
+## Get the urls for the relevant websites we are scraping
+childcare_url <- "https://www.careforkids.com.au/child-care/"
+asking_rents_url <- "https://sqmresearch.com.au/weekly-rents.php?postcode="
+
+## Make tibble with url to scrape (i.e. static website url + dynamic postcode landing page)
+urls <- tibble(postcode = postcodes) %>%
+  mutate(childcare_url = glue::glue("{childcare_url}{postcode}"),
+         rents_url = glue::glue("{asking_rents_url}{postcode}&t=1"))
+
+### Results
+## We purrr::safely for the scraping functions to handle errors
+get_childcare_data_safely <- purrr::safely(get_childcare_data)
+get_rent_safely <- purrr::safely(get_rent)
+
+## Iterate the relevant scraping functions over each postcode
+childcare_data <- purrr::map2(.x = urls$childcare_url, 
+                              .y = urls$postcode,
+                              ~ get_childcare_data_safely(link = .x, postcode = .y))
+
+rent_data <- purrr::map2(.x = urls$rents_url,
+                         .y = urls$postcode,
+                         ~get_rent_safely(link = .x, postcode = .y))
+
+## Get only the scrape yielding results (purrr::safely splits out valid results and errors)
+childcare_data_df <- childcare_data %>%
+  map("result") %>%
+  bind_rows()
+
+rent_data_df <- rent_data %>%
+  map("result") %>%
+  bind_rows()
+
+## Write data
+write.csv(childcare_data_df, "./childcare_data.csv")
+write.csv(rent_data_df, "./rent_data.csv")
