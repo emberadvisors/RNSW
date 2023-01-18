e6283ebe3dfaac09b2802d8be6314f5469e371fc
ember-fbijaksana <faza.bijaksana@emberadvisors.com.au>
2023-01-18 11:30:18 +1100

    Script for creating heatmaps

--- central_west_mapping.R
+++ central_west_mapping.R
@@ -2,28 +2,31 @@ library(strayr)
 library(tidyverse)
 library(sf)
 library(scales)
+library(viridis)
 
-central_west <- c("bathurst|blayney|cabonne|cowra|forbes|lachlan|lithgow|oberon|orange|parkes|weddin")
+central_west <- c("bathurst|blayney|cabonne|cowra|forbes|lachlan|lithgow|mid-western|oberon|orange|parkes|weddin")
 
-vacancies <- rnorm(11, 0.15, 0.01)
+data <- read.csv("./Data/mapping_data.csv") %>%
+  mutate(lga = if_else(lga == "Mid-Western", "Mid-Western Regional", lga))
 
 lga <- strayr::read_absmap("lga2021") %>%
   filter(str_detect(tolower(lga_name_2021), central_west) & !str_detect(tolower(lga_name_2021), "shire")) %>%
   mutate(lga_name_2021 = if_else(lga_name_2021 == "Bathurst Regional", "Bathurst", lga_name_2021))
 
-lga$Vacancies <- vacancies
-lga$labels <- paste0(scales::label_percent(accuracy = 1)(lga$Vacancies), "\n", lga$lga_name_2021)
+lga <- lga %>%
+  left_join(data, by = c("lga_name_2021" = "lga"))
+
+lga$labels <- scales::label_percent(accuracy = 1)(lga$data)
 
 lga %>%
+  filter(metric == "Early education") %>%
   ggplot() +
   geom_sf(aes(geometry = geometry,
-              fill = Vacancies),
+              fill = data),
           colour = "white",
           show.legend = FALSE) +
-  # geom_sf_text(aes(label = labels, geometry = geometry), fun.geometry = st_centroid, colour = "white", size = 4, alpha = 10) +
-  geom_label_repel(data = lga,
-                   aes(x = cent_long,
-                       y = cen_lat,
-                       label = labels)) +
+  geom_sf_text(aes(label = labels, geometry = geometry), fun.geometry = st_centroid, colour = "white", size = 6) +
   theme_void() +
-  coord_sf()
+  coord_sf() +
+  scale_fill_viridis(direction = 1, option = "G", end = 0.7)
+
